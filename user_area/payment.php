8260c09d7fdcafb028615f4888244fed391429c1
Trường Thịnh <you@example.com>
2023-04-07 12:57:58 +0700

    12:57 7/4/2023

--- user_area/payment.php
+++ user_area/payment.php
@@ -1,3 +1,7 @@
+<?php
+    include('../includes/connect.php');
+    include('../functions/common_function.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,10 +15,35 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
     crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- css link -->
-    <link rel="stylesheet" href="./style.css">
     <title>Payment-Page</title>
 </head>
+<style>
+        .img{
+            width: 100%;
+            margin: auto;
+            display: block;
+        }
+    </style>
 <body>
-    
+    <!-- php code to access user -->
+    <?php
+        $user_id = getIPAddress();
+        $get_user = "select * from `user_table` where user_ip = '$user_id'";
+        $result = mysqli_query($con, $get_user);
+        $data_pay = mysqli_fetch_array($result);
+        $user_id = $data_pay['user_id'];
+    ?>
+    <div class="container">
+        <h2 class="text-center text-info">Payment Options</h2>
+        <div class="row d-flex justify-content-center align-items-center">
+            <div class="col-md-6">
+                <a href="https://www.paypal.com" target="_blank"><img src="../images/paypal.jpg" 
+                alt="" class="img"></a>
+            </div>
+            <div class="col-md-6">
+                <a href="./order.php?user_id=<?php echo $user_id ?>"><h2 class="text-center">Pay offline</h2></a>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
